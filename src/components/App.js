af97afb3abda8e8b71b834cdda35f046658301f7
Allison Bohannon <akboh.github@gmail.com>
2022-09-10 18:24:42 -0700

    Add edit form functionality

--- src/components/App.js
+++ src/components/App.js
@@ -80,6 +80,7 @@ function App() {
             />}/>
         <Route path={`${match}/${recipeId}/edit`} element={<EditForm 
             recipeList={recipeList}
+            recipeId={recipeId}
             handleEdit={onEdit}
             />}/>
           <Route path="/" element={<Home recipeList={recipeList}/>} />
